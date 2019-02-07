b0963308081169ec7e7faa91097f3e3e92d27b2a
Douglas Stebila <stebilad@mcmaster.ca>
2019-02-06 19:26:18 -0500

    First attempt at appveyor configuration

---
+++ scripts_windows/build_all.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+SETLOCAL
+SET EL=0
+
+REM CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+REM IF ERRORLEVEL 1 SET EL=1
+
+FOR /D %%K IN (crypto_kem\*) DO (
+  FOR /D %%L IN (%%K\*) DO (
+    cd %%L
+    nmake /f Makefile.Microsoft_nmake clean
+    IF ERRORLEVEL 1 SET EL=2
+    nmake /f Makefile.Microsoft_nmake
+    IF ERRORLEVEL 1 SET EL=3
+    cd ..\..\..
+  )
+)
+
+EXIT /b %EL%
