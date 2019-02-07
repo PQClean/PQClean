7ea8e508a3361c70c24172444cd586fc724f7969
Douglas Stebila <stebilad@mcmaster.ca>
2019-02-06 21:02:34 -0500

    Add Makefiles for dilithium-iii

--- scripts_windows/build_all.bat
+++ scripts_windows/build_all.bat
@@ -5,14 +5,16 @@ SET EL=0
 REM CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 REM IF ERRORLEVEL 1 SET EL=1
 
-FOR /D %%K IN (crypto_kem\*) DO (
-  FOR /D %%L IN (%%K\*) DO (
-    cd %%L
-    nmake /f Makefile.Microsoft_nmake clean
-    IF ERRORLEVEL 1 SET EL=2
-    nmake /f Makefile.Microsoft_nmake
-    IF ERRORLEVEL 1 SET EL=3
-    cd ..\..\..
+FOR /D %%J IN (crypto_kem, crypto_sig) DO (
+  FOR /D %%K IN (%%J\*) DO (
+    FOR /D %%L IN (%%K\*) DO (
+      cd %%L
+      nmake /f Makefile.Microsoft_nmake clean
+      IF ERRORLEVEL 1 SET EL=2
+      nmake /f Makefile.Microsoft_nmake
+      IF ERRORLEVEL 1 SET EL=3
+      cd ..\..\..
+    )
   )
 )
 
