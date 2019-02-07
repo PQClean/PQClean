4f14ce17c9fe85937cdfa643c46cde689ed09590
Douglas Stebila <dstebila@uwaterloo.ca>
2019-02-06 21:14:10 -0500

    Fix Windows build for dilithium-iii

--- scripts_windows/build_all.bat
+++ scripts_windows/build_all.bat
@@ -5,16 +5,14 @@ SET EL=0
 REM CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 REM IF ERRORLEVEL 1 SET EL=1
 
-FOR /D %%J IN (crypto_kem, crypto_sig) DO (
-  FOR /D %%K IN (%%J\*) DO (
-    FOR /D %%L IN (%%K\*) DO (
-      cd %%L
-      nmake /f Makefile.Microsoft_nmake clean
-      IF ERRORLEVEL 1 SET EL=2
-      nmake /f Makefile.Microsoft_nmake
-      IF ERRORLEVEL 1 SET EL=3
-      cd ..\..\..
-    )
+FOR /D %%K IN (crypto_kem\* crypto_sign\*) DO (
+  FOR /D %%L IN (%%K\*) DO (
+    cd %%L
+    nmake /f Makefile.Microsoft_nmake clean
+    IF ERRORLEVEL 1 SET EL=2
+    nmake /f Makefile.Microsoft_nmake
+    IF ERRORLEVEL 1 SET EL=3
+    cd ..\..\..
   )
 )
 
