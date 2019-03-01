e8c4cf949bea6b5f8763a0bbe979552d6e6e0f35
Thom Wiggers <thom@thomwiggers.nl>
2019-03-01 11:44:22 +0100

    Move clang-tidy into python-based tests

---
+++ test/test_linter.py
@@ -0,0 +1,34 @@
+import os
+from glob import glob
+
+import pqclean
+from helpers import run_subprocess
+
+
+def test_clang_tidy():
+    for scheme in pqclean.Scheme.all_schemes():
+        for implementation in scheme.implementations:
+            yield check_tidy, implementation
+
+
+def check_tidy(implementation: pqclean.Implementation):
+    cfiles = glob(os.path.join(implementation.path(), '*.c'))
+    common_files = glob(os.path.join('..', 'common', '*.c'))
+    run_subprocess(['clang-tidy',
+                    '-quiet',
+                    '-header-filter=.*',
+                    *cfiles,
+                    *common_files,
+                    '--',
+                    '-iquote', os.path.join('..', 'common'),
+                    '-iquote', implementation.path(),
+                    ])
+
+
+if __name__ == "__main__":
+    try:
+        import nose2
+        nose2.main()
+    except ImportError:
+        import nose
+        nose.runmodule()
