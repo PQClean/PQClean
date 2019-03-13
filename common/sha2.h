d325e39c681c9d56bd981041603e650eb7b7a818
Joost Rijneveld <joost@joostrijneveld.nl>
2019-03-13 17:23:30 +0100

    Add documentation for SHA2 blockwise functions

--- common/sha2.h
+++ common/sha2.h
@@ -4,6 +4,10 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/* The incremental API allows hashing of individual input blocks; these blocks
+    must be exactly 64 bytes each.
+    Use the 'finalize' functions for any remaining bytes (possibly over 64). */
+
 void sha224_inc_init(uint8_t *state);
 void sha224_inc_blocks(uint8_t *state, const uint8_t *in, size_t inblocks);
 void sha224_inc_finalize(uint8_t *out, uint8_t *state, const uint8_t *in, size_t inlen);
