d1fd8aa934e8614e6b072fa6751ce65d216113db
Thom Wiggers <thom@thomwiggers.nl>
2019-04-05 17:04:37 +0200

    Check the specified sizes of the metadata match api.h consts
    Fixes #88

---
+++ test/crypto_sign/printparams.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "api.h"
+
+#define PASTER(x, y) x##_##y
+#define EVALUATOR(x, y) PASTER(x, y)
+#define NAMESPACE(fun) EVALUATOR(PQCLEAN_NAMESPACE, fun)
+
+int main() {
+    printf("{\n");
+    printf("\t\"CRYPTO_SECRETKEYBYTES\": %u,\n",  NAMESPACE(CRYPTO_SECRETKEYBYTES));
+    printf("\t\"CRYPTO_PUBLICKEYBYTES\": %u,\n",  NAMESPACE(CRYPTO_PUBLICKEYBYTES));
+    printf("\t\"CRYPTO_BYTES\": %u\n}\n",         NAMESPACE(CRYPTO_BYTES));
+}
---
+++ test/test_metadata_sizes.py
@@ -0,0 +1,50 @@
+import json
+import os
+
+import pqclean
+import helpers
+
+
+def test_metadata_sizes():
+    for scheme in pqclean.Scheme.all_schemes():
+        for implementation in scheme.implementations:
+            yield check_metadata_sizes, implementation
+
+
+def check_metadata_sizes(implementation):
+    metadata = implementation.scheme.metadata()
+    helpers.make('printparams',
+                 TYPE=implementation.scheme.type,
+                 SCHEME=implementation.scheme.name,
+                 IMPLEMENTATION=implementation.name,
+                 working_dir=os.path.join('..', 'test'))
+
+    out = helpers.run_subprocess(
+            [os.path.join('..', 'bin', 'printparams_{}_{}{}'.format(
+                implementation.scheme.name,
+                implementation.name,
+                '.exe' if os.name == 'nt' else ''
+            ))],
+            os.path.join('..', 'bin'),
+        ).replace('\r', '')
+
+    parsed = json.loads(out)
+
+    assert parsed['CRYPTO_SECRETKEYBYTES'] == metadata['length-secret-key']
+    assert parsed['CRYPTO_PUBLICKEYBYTES'] == metadata['length-public-key']
+
+    if implementation.scheme.type == 'kem':
+        assert (
+            parsed['CRYPTO_CIPHERTEXTBYTES'] == metadata['length-ciphertext'])
+        assert parsed['CRYPTO_BYTES'] == metadata['length-shared-secret']
+    else:
+        assert parsed['CRYPTO_BYTES'] == metadata['length-signature']
+
+
+if __name__ == '__main__':
+    try:
+        import nose2
+        nose2.main()
+    except ImportError:
+        import nose
+        nose.runmodule()
