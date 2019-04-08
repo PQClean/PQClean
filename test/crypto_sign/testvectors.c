87a3c6ab39b2b15e09a78e1210fcba3e8b00d001
Joost Rijneveld <joost@joostrijneveld.nl>
2019-04-08 10:21:32 +0200

    Add functest/testvectors for detached signatures

--- test/crypto_sign/testvectors.c
+++ test/crypto_sign/testvectors.c
@@ -28,6 +28,8 @@ static void printbytes(const uint8_t *x, size_t xlen) {
 #define crypto_sign_keypair NAMESPACE(crypto_sign_keypair)
 #define crypto_sign NAMESPACE(crypto_sign)
 #define crypto_sign_open NAMESPACE(crypto_sign_open)
+#define crypto_sign_signature NAMESPACE(crypto_sign_signature)
+#define crypto_sign_verify NAMESPACE(crypto_sign_verify)
 
 int main(void) {
     uint8_t sk[CRYPTO_SECRETKEYBYTES];
@@ -35,8 +37,10 @@ int main(void) {
 
     uint8_t mi[MAXMLEN];
     uint8_t sm[MAXMLEN + CRYPTO_BYTES];
+    uint8_t sig[CRYPTO_BYTES];
 
     size_t smlen;
+    size_t siglen;
     size_t mlen;
 
     int r;
@@ -52,12 +56,15 @@ int main(void) {
         printbytes(sk, CRYPTO_SECRETKEYBYTES);
 
         crypto_sign(sm, &smlen, mi, i, sk);
+        crypto_sign_signature(sig, &siglen, mi, i, sk);
 
         printbytes(sm, smlen);
+        printbytes(sig, siglen);
 
         // By relying on m == sm we prevent having to allocate CRYPTO_BYTES
         // twice
         r = crypto_sign_open(sm, &mlen, sm, smlen, pk);
+        r |= crypto_sign_verify(sig, siglen, mi, i, pk);
 
         if (r) {
             printf("ERROR: signature verification failed\n");
