7ece69cd110b2a1edba9a0f36c27e5313defd1e1
Peter Schwabe <peter@cryptojedi.org>
2019-04-09 16:37:25 +0200

    Added missing files

--- crypto_kem/kyber768/clean/symmetric.h
+++ crypto_kem/kyber768/clean/symmetric.h
@@ -1,16 +1,16 @@
 #ifndef SYMMETRIC_H
 #define SYMMETRIC_H
 
-#include "fips202.h"
 #include "params.h"
+#include "fips202.h"
 
 typedef struct {
-    uint64_t s[25];
+  uint64_t s[25];
 } keccak_state;
 
 void PQCLEAN_KYBER768_CLEAN_kyber_shake128_absorb(keccak_state *s, const unsigned char *input, unsigned char x, unsigned char y);
-void PQCLEAN_KYBER768_CLEAN_kyber_shake128_squeezeblocks(unsigned char *output, size_t nblocks, keccak_state *s);
-void PQCLEAN_KYBER768_CLEAN_shake256_prf(unsigned char *output, size_t outlen, const unsigned char *key, unsigned char nonce);
+void PQCLEAN_KYBER768_CLEAN_kyber_shake128_squeezeblocks(unsigned char *output, unsigned long long nblocks, keccak_state *s);
+void PQCLEAN_KYBER768_CLEAN_shake256_prf(unsigned char *output, unsigned long long outlen, const unsigned char *key, const unsigned char nonce);
 
 #define hash_h(OUT, IN, INBYTES) sha3_256(OUT, IN, INBYTES)
 #define hash_g(OUT, IN, INBYTES) sha3_512(OUT, IN, INBYTES)
