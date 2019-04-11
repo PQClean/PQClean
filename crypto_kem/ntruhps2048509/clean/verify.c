d91b4ce105b794d4e51f756aa4a4fd96cc6a84e9
Douglas Stebila <dstebila@uwaterloo.ca>
2019-04-10 20:49:54 -0400

    Initial ntruhps2048509 implementation

---
+++ crypto_kem/ntruhps2048509/clean/verify.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdint.h>
+
+/* returns 0 for equal strings, 1 for non-equal strings */
+int PQCLEAN_NTRUHPS2048509_CLEAN_verify(const unsigned char *a, const unsigned char *b, size_t len)
+{
+  uint64_t r;
+  size_t i;
+  r = 0;
+  
+  for(i=0;i<len;i++)
+    r |= a[i] ^ b[i];
+ 
+  r = (-r) >> 63;
+  return r;
+}
+
+/* b = 1 means mov, b = 0 means don't mov*/
+void PQCLEAN_NTRUHPS2048509_CLEAN_cmov(unsigned char *r, const unsigned char *x, size_t len, unsigned char b)
+{
+  size_t i;
+
+  b = -b;
+  for(i=0;i<len;i++)
+    r[i] ^= b & (x[i] ^ r[i]);
+}
