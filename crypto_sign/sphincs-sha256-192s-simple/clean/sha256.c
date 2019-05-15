00639575b03ce43fd2d434d204aec5ceff7c33ab
Joost Rijneveld <joost@joostrijneveld.nl>
2019-05-15 10:23:33 +0200

    Remove redundant array decl SPHINCS+

--- crypto_sign/sphincs-sha256-192s-simple/clean/sha256.c
+++ crypto_sign/sphincs-sha256-192s-simple/clean/sha256.c
@@ -50,8 +50,6 @@ void PQCLEAN_SPHINCSSHA256192SSIMPLE_CLEAN_mgf1(
     }
 }
 
-uint8_t PQCLEAN_SPHINCSSHA256192SSIMPLE_CLEAN_state_seeded[40];
-
 /**
  * Absorb the constant pub_seed using one round of the compression function
  * This initializes state_seeded, which can then be reused in thash
