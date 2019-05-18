82fdce64a2e8c9b6688d4b5f0f0f1ec235c3ad82
Douglas Stebila <dstebila@uwaterloo.ca>
2019-05-18 11:27:45 +0200

    Macro for loop length

--- crypto_kem/newhope512cca/clean/poly.c
+++ crypto_kem/newhope512cca/clean/poly.c
@@ -151,7 +151,7 @@ void PQCLEAN_NEWHOPE512CCA_CLEAN_poly_decompress(poly *r, const unsigned char *a
 **************************************************/
 void PQCLEAN_NEWHOPE512CCA_CLEAN_poly_frommsg(poly *r, const unsigned char *msg) {
     unsigned int i, j, mask;
-    for (i = 0; i < 32; i++) { // XXX: MACRO for 32
+    for (i = 0; i < NEWHOPE_SYMBYTES; i++) {
         for (j = 0; j < 8; j++) {
             mask = -((msg[i] >> j) & 1);
             r->coeffs[8 * i + j +  0] = mask & (NEWHOPE_Q / 2);
