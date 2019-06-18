6aafbd56f2d1f384452f1c209d6e5fb43771b58b
Matthias J. Kannwischer <matthias@kannwischer.eu>
2019-06-18 16:37:25 +0200

    add firesaber

---
+++ crypto_kem/firesaber/clean/poly_mul.c
@@ -0,0 +1,22 @@
+#include "poly_mul.h"
+#include "SABER_params.h"
+#include <stdint.h>
+#include <string.h>
+
+
+void PQCLEAN_FIRESABER_CLEAN_pol_mul(const uint16_t *a, const uint16_t *b, uint16_t *res, uint16_t p, uint32_t n) {
+    // Polynomial multiplication using the schoolbook method, c[x] = a[x]*b[x]
+
+    // normal multiplication
+    uint16_t c[2 * SABER_N] = {0};
+    for (size_t i = 0; i < SABER_N; i++) {
+        for (size_t j = 0; j < SABER_N; j++) {
+            c[i + j] += a[i] * b[j];
+        }
+    }
+
+    // reduction
+    for (size_t i = n; i < 2 * n; i++) {
+        res[i - n] = (c[i - n] - c[i]) & (p - 1);
+    }
+}
