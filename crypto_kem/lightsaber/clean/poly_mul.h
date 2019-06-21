931f4669371a1488f7737594032715e7f7167aec
Matthias J. Kannwischer <matthias@kannwischer.eu>
2019-06-21 15:05:59 +0200

    switch to the polymul from the submission

--- crypto_kem/lightsaber/clean/poly_mul.h
+++ crypto_kem/lightsaber/clean/poly_mul.h
@@ -4,6 +4,6 @@
 #include "SABER_params.h"
 #include <stdint.h>
 
-void PQCLEAN_LIGHTSABER_CLEAN_pol_mul(const uint16_t *a, const uint16_t *b, uint16_t *res, uint16_t p, uint32_t n);
+void PQCLEAN_LIGHTSABER_CLEAN_pol_mul(uint16_t *a, uint16_t *b, uint16_t *res, uint16_t p, uint32_t n);
 
 #endif
