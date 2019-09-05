aa46b5239d84c48081d0680b338fdf4b26359a69
Thom Wiggers <thomwiggers@users.noreply.github.com>
2019-09-05 10:08:42 +0200

    Enable valgrind exit errorcode and remove Rainbow (#223)
    * Enable valgrind exit errorcode
    
    It seems we were not doing anything with the valgrind results... D:
    
    * Use --exit-on-first-error if available
    
    * Revert "Merge pull request #182 from PQClean/rainbow"
    
    This reverts commit 8a008417fe988b48c8acc4aefc9c299a4d3c1645, reversing
    changes made to d4b612fe9aa2d43280bd6fea9b619d4dea2c2903.

--- test/test_valgrind.py
+++ test/test_valgrind.py
@@ -2,14 +2,26 @@
 Runs the test files under valgrind to detect memory problems
 """
 
+import functools
 import os
 import platform
+import re
 import unittest
 
 import pytest
 
 import helpers
 import pqclean
+
+
+@functools.lru_cache()
+def valgrind_supports_exit_early():
+    """Checks if we support early exit from valgrind"""
+    version = helpers.run_subprocess(['valgrind', '--version'])
+    match = re.match(r'valgrind-(\d)\.(\d+).*', version)
+    if match:
+        return int(match.group(2)) >= 14
+    return False
 
 
 @pytest.mark.parametrize(
@@ -38,7 +50,15 @@ def test_valgrind(implementation: pqclean.Implementation, impl_path, test_dir,
                  working_dir=os.path.join(test_dir, 'test'))
     functest_name = './functest_{}_{}'.format(implementation.scheme.name,
                                               implementation.name)
-    helpers.run_subprocess(['valgrind', functest_name], dest_dir)
+    helpers.run_subprocess(
+        ['valgrind',
+         '--error-exitcode=1',
+         *(['--exit-on-first-error=yes']
+           if valgrind_supports_exit_early()
+           else []),
+         '--max-stackframe=20933064',
+         functest_name],
+        dest_dir)
     destr()
 
 
