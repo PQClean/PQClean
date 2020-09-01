22c4feb174df59cc4ebcef981d223621087a7d89
John M. Schanck <jschanck@uwaterloo.ca>
2020-09-01 16:18:21 -0400

    NTRU Prime: avoid unsigned unary negation

--- crypto_kem/sntrup653/clean/crypto_core_wforcesntrup653.c
+++ crypto_kem/sntrup653/clean/crypto_core_wforcesntrup653.c
@@ -14,7 +14,7 @@ typedef int8 small;
 static int int16_nonzero_mask(int16 x) {
     uint16 u = (uint16) x; /* 0, else 1...65535 */
     uint32 v = u; /* 0, else 1...65535 */
-    v = -v; /* 0, else 2^32-65535...2^32-1 */
+    v = ~v + 1; /* 0, else 2^32-65535...2^32-1 */
     v >>= 31; /* 0, else 1 */
     return -(int) v; /* 0, else -1 */
 }
