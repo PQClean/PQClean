7c342d2039ab8131a43575709892953a7529495d
John M. Schanck <jschanck@uwaterloo.ca>
2020-09-07 09:27:39 -0400

    NTRU Prime: Get random bytes in one shot and use corresponding KATs

--- crypto_kem/sntrup761/avx2/kem.c
+++ crypto_kem/sntrup761/avx2/kem.c
@@ -73,25 +73,12 @@ static void Hash(unsigned char *out, const unsigned char *in, int inlen) {
 
 /* ----- higher-level randomness */
 
-static uint32 urandom32(void) {
-    unsigned char c[4];
-    uint32 out[4];
-
-    randombytes(c, 4);
-    out[0] = (uint32)c[0];
-    out[1] = ((uint32)c[1]) << 8;
-    out[2] = ((uint32)c[2]) << 16;
-    out[3] = ((uint32)c[3]) << 24;
-    return out[0] + out[1] + out[2] + out[3];
-}
-
 static void Short_random(small *out) {
     uint32 L[ppadsort];
     int i;
 
-    for (i = 0; i < ppadsort; ++i) {
-        L[i] = urandom32();
-    }
+    randombytes((unsigned char *) L, 4 * p);
+    crypto_decode_pxint32(L, (unsigned char *) L);
     for (i = 0; i < w; ++i) {
         L[i] = L[i] & (uint32) - 2;
     }
@@ -111,9 +98,8 @@ static void Small_random(small *out) {
     uint32 L[p];
     int i;
 
-    for (i = 0; i < p; ++i) {
-        L[i] = urandom32();
-    }
+    randombytes((unsigned char *) L, sizeof L);
+    crypto_decode_pxint32(L, (unsigned char *) L);
     for (i = 0; i < p; ++i) {
         out[i] = (small) ((((L[i] & 0x3fffffff) * 3) >> 30) - 1);
     }
