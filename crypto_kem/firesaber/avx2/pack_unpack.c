f8503cbd7158049fce79cc26fe784f17179226ea
John M. Schanck <jschanck@uwaterloo.ca>
2020-10-28 23:31:01 -0400

    simplify pack_unpack.c

--- crypto_kem/firesaber/avx2/pack_unpack.c
+++ crypto_kem/firesaber/avx2/pack_unpack.c
@@ -8,23 +8,24 @@ void PQCLEAN_FIRESABER_AVX2_POLT2BS(uint8_t bytes[SABER_SCALEBYTES_KEM], const p
     const uint16_t *in = data->coeffs;
     uint8_t *out = bytes;
     for (j = 0; j < SABER_N / 4; j++) {
-        out[0] = (in[0] & 0x3f) | ((in[1] & 0x03) << 6);
-        out[1] = ((in[1] >> 2) & 0x0f) | ((in[2] & 0x0f) << 4);
-        out[2] = ((in[2] >> 4) & 0x03) | ((in[3] & 0x3f) << 2);
+        out[0] = (in[0] & 0x3f) | (in[1] << 6);
+        out[1] = ((in[1] >> 2) & 0x0f) | (in[2] << 4);
+        out[2] = ((in[2] >> 4) & 0x03) | (in[3] << 2);
         in += 4;
         out += 3;
     }
 }
 
 void PQCLEAN_FIRESABER_AVX2_BS2POLT(poly *data, const uint8_t bytes[SABER_SCALEBYTES_KEM]) {
+    /* This function does not reduce its output mod T */
     size_t j;
     const uint8_t *in = bytes;
     uint16_t *out = data->coeffs;
     for (j = 0; j < SABER_N / 4; j++) {
-        out[0] = in[0] & 0x3f;
-        out[1] = ((in[0] >> 6) & 0x03) | ((in[1] & 0x0f) << 2);
-        out[2] = ((in[1] & 0xff) >> 4) | ((in[2] & 0x03) << 4);
-        out[3] = ((in[2] & 0xff) >> 2);
+        out[0] = in[0];
+        out[1] = (in[0] >> 6) | (in[1] << 2);
+        out[2] = (in[1] >> 4) | (in[2] << 4);
+        out[3] = (in[2] >> 2);
         in += 3;
         out += 4;
     }
@@ -35,37 +36,38 @@ static void POLq2BS(uint8_t bytes[SABER_POLYBYTES], const poly *data) {
     const uint16_t *in = data->coeffs;
     uint8_t *out = bytes;
     for (j = 0; j < SABER_N / 8; j++) {
-        out[0] = (in[0] & (0xff));
-        out[1] = ((in[0] >> 8) & 0x1f) | ((in[1] & 0x07) << 5);
-        out[2] = ((in[1] >> 3) & 0xff);
-        out[3] = ((in[1] >> 11) & 0x03) | ((in[2] & 0x3f) << 2);
-        out[4] = ((in[2] >> 6) & 0x7f) | ((in[3] & 0x01) << 7);
-        out[5] = ((in[3] >> 1) & 0xff);
-        out[6] = ((in[3] >> 9) & 0x0f) | ((in[4] & 0x0f) << 4);
-        out[7] = ((in[4] >> 4) & 0xff);
-        out[8] = ((in[4] >> 12) & 0x01) | ((in[5] & 0x7f) << 1);
-        out[9] = ((in[5] >> 7) & 0x3f) | ((in[6] & 0x03) << 6);
-        out[10] = ((in[6] >> 2) & 0xff);
-        out[11] = ((in[6] >> 10) & 0x07) | ((in[7] & 0x1f) << 3);
-        out[12] = ((in[7] >> 5) & 0xff);
+        out[0] = in[0];
+        out[1] = ((in[0] >> 8) & 0x1f) | (in[1] << 5);
+        out[2] = in[1] >> 3;
+        out[3] = ((in[1] >> 11) & 0x03) | (in[2] << 2);
+        out[4] = ((in[2] >> 6) & 0x7f) | (in[3] << 7);
+        out[5] = in[3] >> 1;
+        out[6] = ((in[3] >> 9) & 0x0f) | (in[4] << 4);
+        out[7] = in[4] >> 4;
+        out[8] = ((in[4] >> 12) & 0x01) | (in[5] << 1);
+        out[9] = ((in[5] >> 7) & 0x3f) | (in[6] << 6);
+        out[10] = in[6] >> 2;
+        out[11] = ((in[6] >> 10) & 0x07) | (in[7] << 3);
+        out[12] = in[7] >> 5;
         in += 8;
         out += 13;
     }
 }
 
 static void BS2POLq(poly *data, const uint8_t bytes[SABER_POLYBYTES]) {
+    /* This function does not reduce its output mod Q */
     size_t j;
     const uint8_t *in = bytes;
     uint16_t *out = data->coeffs;
     for (j = 0; j < SABER_N / 8; j++) {
-        out[0] = (in[0] & (0xff)) | ((in[1] & 0x1f) << 8);
-        out[1] = (in[1] >> 5 & (0x07)) | ((in[2] & 0xff) << 3) | ((in[3] & 0x03) << 11);
-        out[2] = (in[3] >> 2 & (0x3f)) | ((in[4] & 0x7f) << 6);
-        out[3] = (in[4] >> 7 & (0x01)) | ((in[5] & 0xff) << 1) | ((in[6] & 0x0f) << 9);
-        out[4] = (in[6] >> 4 & (0x0f)) | ((in[7] & 0xff) << 4) | ((in[8] & 0x01) << 12);
-        out[5] = (in[8] >> 1 & (0x7f)) | ((in[9] & 0x3f) << 7);
-        out[6] = (in[9] >> 6 & (0x03)) | ((in[10] & 0xff) << 2) | ((in[11] & 0x07) << 10);
-        out[7] = (in[11] >> 3 & (0x1f)) | ((in[12] & 0xff) << 5);
+        out[0] = (in[0]) | (in[1] << 8);
+        out[1] = (in[1] >> 5) | (in[2] << 3) | (in[3] << 11);
+        out[2] = (in[3] >> 2) | (in[4] << 6);
+        out[3] = (in[4] >> 7) | (in[5] << 1) | (in[6] << 9);
+        out[4] = (in[6] >> 4) | (in[7] << 4) | (in[8] << 12);
+        out[5] = (in[8] >> 1) | (in[9] << 7);
+        out[6] = (in[9] >> 6) | (in[10] << 2) | (in[11] << 10);
+        out[7] = (in[11] >> 3) | (in[12] << 5);
         in += 13;
         out += 8;
     }
@@ -76,11 +78,11 @@ static void POLp2BS(uint8_t bytes[SABER_POLYCOMPRESSEDBYTES], const poly *data) 
     const uint16_t *in = data->coeffs;
     uint8_t *out = bytes;
     for (j = 0; j < SABER_N / 4; j++) {
-        out[0] = (in[0] & (0xff));
-        out[1] = ((in[0] >> 8) & 0x03) | ((in[1] & 0x3f) << 2);
-        out[2] = ((in[1] >> 6) & 0x0f) | ((in[2] & 0x0f) << 4);
-        out[3] = ((in[2] >> 4) & 0x3f) | ((in[3] & 0x03) << 6);
-        out[4] = ((in[3] >> 2) & 0xff);
+        out[0] = in[0];
+        out[1] = ((in[0] >> 8) & 0x03) | (in[1] << 2);
+        out[2] = ((in[1] >> 6) & 0x0f) | (in[2] << 4);
+        out[3] = ((in[2] >> 4) & 0x3f) | (in[3] << 6);
+        out[4] = in[3] >> 2;
         in += 4;
         out += 5;
     }
@@ -91,10 +93,10 @@ static void BS2POLp(poly *data, const uint8_t bytes[SABER_POLYCOMPRESSEDBYTES]) 
     const uint8_t *in = bytes;
     uint16_t *out = data->coeffs;
     for (j = 0; j < SABER_N / 4; j++) {
-        out[0] = (in[0] & (0xff)) | ((in[1] & 0x03) << 8);
-        out[1] = ((in[1] >> 2) & (0x3f)) | ((in[2] & 0x0f) << 6);
-        out[2] = ((in[2] >> 4) & (0x0f)) | ((in[3] & 0x3f) << 4);
-        out[3] = ((in[3] >> 6) & (0x03)) | ((in[4] & 0xff) << 2);
+        out[0] = in[0] | (in[1] << 8);
+        out[1] = (in[1] >> 2) | (in[2] << 6);
+        out[2] = (in[2] >> 4) | (in[3] << 4);
+        out[3] = (in[3] >> 6) | (in[4] << 2);
         in += 5;
         out += 4;
     }
