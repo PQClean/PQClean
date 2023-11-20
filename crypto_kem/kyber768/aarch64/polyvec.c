5562555e7dc549fc53a3a87c581148f5b1484a57
vincentvbh <vincentvbh7@gmail.com>
2023-11-20 18:22:46 +0100

    tweak preprocessing

--- crypto_kem/kyber768/aarch64/polyvec.c
+++ crypto_kem/kyber768/aarch64/polyvec.c
@@ -22,31 +22,6 @@
 void polyvec_compress(uint8_t r[KYBER_POLYVECCOMPRESSEDBYTES], const int16_t a[KYBER_K][KYBER_N]) {
     unsigned int i, j, k;
 
-    #if (KYBER_POLYVECCOMPRESSEDBYTES == (KYBER_K * 352))
-    uint16_t t[8];
-    for (i = 0; i < KYBER_K; i++) {
-        for (j = 0; j < KYBER_N / 8; j++) {
-            for (k = 0; k < 8; k++) {
-                t[k]  = a[i][8 * j + k];
-                t[k] += ((int16_t)t[k] >> 15) & KYBER_Q;
-                t[k]  = ((((uint32_t)t[k] << 11) + KYBER_Q / 2) / KYBER_Q) & 0x7ff;
-            }
-
-            r[ 0] = (t[0] >>  0);
-            r[ 1] = (t[0] >>  8) | (t[1] << 3);
-            r[ 2] = (t[1] >>  5) | (t[2] << 6);
-            r[ 3] = (t[2] >>  2);
-            r[ 4] = (t[2] >> 10) | (t[3] << 1);
-            r[ 5] = (t[3] >>  7) | (t[4] << 4);
-            r[ 6] = (t[4] >>  4) | (t[5] << 7);
-            r[ 7] = (t[5] >>  1);
-            r[ 8] = (t[5] >>  9) | (t[6] << 2);
-            r[ 9] = (t[6] >>  6) | (t[7] << 5);
-            r[10] = (t[7] >>  3);
-            r += 11;
-        }
-    }
-    #elif (KYBER_POLYVECCOMPRESSEDBYTES == (KYBER_K * 320))
     uint16_t t[4];
     for (i = 0; i < KYBER_K; i++) {
         for (j = 0; j < KYBER_N / 4; j++) {
@@ -64,9 +39,6 @@ void polyvec_compress(uint8_t r[KYBER_POLYVECCOMPRESSEDBYTES], const int16_t a[K
             r += 5;
         }
     }
-    #else
-#error "KYBER_POLYVECCOMPRESSEDBYTES needs to be in {320*KYBER_K, 352*KYBER_K}"
-    #endif
 }
 
 /*************************************************
@@ -82,26 +54,6 @@ void polyvec_compress(uint8_t r[KYBER_POLYVECCOMPRESSEDBYTES], const int16_t a[K
 void polyvec_decompress(int16_t r[KYBER_K][KYBER_N], const uint8_t a[KYBER_POLYVECCOMPRESSEDBYTES]) {
     unsigned int i, j, k;
 
-    #if (KYBER_POLYVECCOMPRESSEDBYTES == (KYBER_K * 352))
-    uint16_t t[8];
-    for (i = 0; i < KYBER_K; i++) {
-        for (j = 0; j < KYBER_N / 8; j++) {
-            t[0] = (a[0] >> 0) | ((uint16_t)a[ 1] << 8);
-            t[1] = (a[1] >> 3) | ((uint16_t)a[ 2] << 5);
-            t[2] = (a[2] >> 6) | ((uint16_t)a[ 3] << 2) | ((uint16_t)a[4] << 10);
-            t[3] = (a[4] >> 1) | ((uint16_t)a[ 5] << 7);
-            t[4] = (a[5] >> 4) | ((uint16_t)a[ 6] << 4);
-            t[5] = (a[6] >> 7) | ((uint16_t)a[ 7] << 1) | ((uint16_t)a[8] << 9);
-            t[6] = (a[8] >> 2) | ((uint16_t)a[ 9] << 6);
-            t[7] = (a[9] >> 5) | ((uint16_t)a[10] << 3);
-            a += 11;
-
-            for (k = 0; k < 8; k++) {
-                r[i][8 * j + k] = ((uint32_t)(t[k] & 0x7FF) * KYBER_Q + 1024) >> 11;
-            }
-        }
-    }
-    #elif (KYBER_POLYVECCOMPRESSEDBYTES == (KYBER_K * 320))
     uint16_t t[4];
     for (i = 0; i < KYBER_K; i++) {
         for (j = 0; j < KYBER_N / 4; j++) {
@@ -116,9 +68,6 @@ void polyvec_decompress(int16_t r[KYBER_K][KYBER_N], const uint8_t a[KYBER_POLYV
             }
         }
     }
-    #else
-#error "KYBER_POLYVECCOMPRESSEDBYTES needs to be in {320*KYBER_K, 352*KYBER_K}"
-    #endif
 }
 
 /*************************************************
