a05c19d7a892dd41d7b4bbbaa6948c054bf23086
Thom Wiggers <thom@thomwiggers.nl>
2023-12-20 09:39:19 +0100

    Backport Kyber fix

--- crypto_kem/kyber768/clean/poly.c
+++ crypto_kem/kyber768/clean/poly.c
@@ -129,14 +129,19 @@ void PQCLEAN_KYBER768_CLEAN_poly_frommsg(poly *r, const uint8_t msg[KYBER_INDCPA
 **************************************************/
 void PQCLEAN_KYBER768_CLEAN_poly_tomsg(uint8_t msg[KYBER_INDCPA_MSGBYTES], const poly *a) {
     size_t i, j;
-    uint16_t t;
+    uint32_t t;
 
     for (i = 0; i < KYBER_N / 8; i++) {
         msg[i] = 0;
         for (j = 0; j < 8; j++) {
             t  = a->coeffs[8 * i + j];
-            t += ((int16_t)t >> 15) & KYBER_Q;
-            t  = (((t << 1) + KYBER_Q / 2) / KYBER_Q) & 1;
+            //t += ((int16_t)t >> 15) & KYBER_Q;
+            //t  = (((t << 1) + KYBER_Q / 2) / KYBER_Q) & 1;
+            t <<= 1;
+            t += 1665;
+            t *= 80635;
+            t >>= 28;
+            t &= 1;
             msg[i] |= t << j;
         }
     }
