e1cc266b879b6796141422819a1d6ba6ac2425bb
Thom Wiggers <thom@thomwiggers.nl>
2024-01-25 14:10:02 +0100

    Help debug astyle issues

--- test/test_format.py
+++ test/test_format.py
@@ -1,6 +1,7 @@
 import os
 import platform
 import pytest
+import functools
 
 import helpers
 import pqclean
@@ -10,11 +11,26 @@ __tracebackhide__ = True
 if platform.machine() == "ppc":
     pytest.skip("Skipping this test on PowerPC to save cycles.", allow_module_level=True)
 
+
+__astyle_version_result = None
+
+def _get_astyle_version() -> str:
+    """Get the AStyle version number"""
+    # functools.lru_cache doesn't work because we want to print to stdout each time.
+    global __astyle_version_result
+
+    if __astyle_version_result is None:
+        __astyle_version_result = helpers.run_subprocess(['astyle', '--version'])
+    else:
+        print(__astyle_version_result)
+    return __astyle_version_result
+
+
 helpers.ensure_available('astyle')
 
 # Check AStyle version
 def version_check():
-    result = helpers.run_subprocess(['astyle', '--version'])
+    result = _get_astyle_version()
     if ("Artistic Style Version 3.4.8" in result or
         "Artistic Style Version 3.4.9" in result or
         "Artistic Style Version 3.4.10" in result):
@@ -22,7 +38,7 @@ def version_check():
     return "Artistic Style Version 3.4" in result
 
 if not version_check() and "CI" not in os.environ:
-    pytest.skip("Incompatible AStyle version (need 3.4.x) (not 3.4.{8,9})", allow_module_level=True)
+    pytest.skip("Incompatible AStyle version (need 3.4.x) (not 3.4.{8-10})", allow_module_level=True)
 
 @pytest.mark.parametrize(
     'implementation',
@@ -33,6 +49,8 @@ if not version_check() and "CI" not in os.environ:
 def test_format(implementation: pqclean.Implementation):
     cfiles = implementation.cfiles()
     hfiles = implementation.hfiles()
+    # Triggers a print
+    _get_astyle_version()
     result = helpers.run_subprocess(
         ['astyle',
          '--dry-run',
