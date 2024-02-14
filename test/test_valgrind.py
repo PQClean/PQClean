0edae0320776cb06adc71d08811152b9d64ccfa2
Spencer Wilson <spencer.wilson@uwaterloo.ca>
2024-02-14 15:49:55 -0500

    Add interop framework for sanitizer and Valgrind

--- test/test_valgrind.py
+++ test/test_valgrind.py
@@ -40,7 +40,26 @@ def test_valgrind(implementation: pqclean.Implementation, impl_path, test_dir,
 
     dest_dir = os.path.join(test_dir, 'bin')
 
-    helpers.make(TYPE=implementation.scheme.type,
+    # handle Falcon PADDED and COMPACT interop testing
+    if implementation.scheme.name.startswith("falcon-"):
+        if implementation.scheme.name.endswith("-padded"):
+            # strip off "-padded" suffix to get interop scheme name
+            interop_src = pqclean.Implementation.by_name(implementation.scheme.name[:-len("-padded")], implementation.name).path()
+        else:
+            # add "-padded" suffix to get interop scheme name
+            interop_src = pqclean.Implementation.by_name(implementation.scheme.name + "-padded", implementation.name).path()
+        interop_dir = helpers.add_interop_files(interop_src, os.path.join(impl_path, '..'))
+        helpers.make(TYPE=implementation.scheme.type,
+                 SCHEME=implementation.scheme.name,
+                 SCHEME_DIR=os.path.abspath(impl_path),
+                 IMPLEMENTATION=implementation.name,
+                 INTEROP_DIR=interop_dir,
+                 DEST_DIR=dest_dir,
+                 EXTRAFLAGS="-gdwarf-4 -DPQCLEAN_USE_VALGRIND",
+                 NTESTS=1,
+                 working_dir=os.path.join(test_dir, 'test'))
+    else:
+        helpers.make(TYPE=implementation.scheme.type,
                  SCHEME=implementation.scheme.name,
                  SCHEME_DIR=os.path.abspath(impl_path),
                  IMPLEMENTATION=implementation.name,
