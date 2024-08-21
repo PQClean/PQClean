5274b58d90a6228e0d17d01342e259920c5a719d
Matthias J. Kannwischer <matthias@kannwischer.eu>
2024-08-21 19:57:49 +0800

    another typo

--- crypto_kem/ml-kem-768/aarch64/api.h
+++ crypto_kem/ml-kem-768/aarch64/api.h
@@ -19,6 +19,6 @@ int PQCLEAN_MLKEM768_AARCH64_crypto_kem_keypair(uint8_t *pk, uint8_t *sk);
 
 int PQCLEAN_MLKEM768_AARCH64_crypto_kem_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk);
 
-int PQCLEAN_MLEM768_AARCH64_crypto_kem_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk);
+int PQCLEAN_MLKEM768_AARCH64_crypto_kem_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk);
 
 #endif
