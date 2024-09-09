67fbb3efe30f1f28593f811bc0c4fcb382326471
Thom Wiggers <thom@thomwiggers.nl>
2024-09-09 14:14:50 +0200

    Add prevent branching __asm__ macro trick

--- test/common/compat.h
+++ test/common/compat.h
@@ -1,3 +1,4 @@
 /** Empty on purpose so preprocessor / compiler-specific stuff does not choke test_char.py **/
 
 #define PQCLEAN_VLA(__t,__x,__s) __t __x[__s]
+#define PQCLEAN_PREVENT_BRANCH_HACK(b)
