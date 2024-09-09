7174b1c45b14f26a140c17c16533e4407a178182
Thom Wiggers <thom@thomwiggers.nl>
2024-09-09 14:14:50 +0200

    Add prevent branching __asm__ macro trick

--- common/compat.h
+++ common/compat.h
@@ -54,4 +54,20 @@ UNALIGNED_VECTOR_POLYFILL_GCC
 # define PQCLEAN_VLA(__t,__x,__s) __t __x[__s]
 #endif
 
+/*********************************
+ * Prevent branching on variable *
+ *********************************/
+
+#if defined(__GNUC__) || defined(__clang__)
+  // Prevent the compiler from
+  //    1) inferring that b is 0/1-valued, and
+  //    2) handling the two cases with a branch.
+  // This is not necessary when verify.c and kem.c are separate translation
+  // units, but we expect that downstream consumers will copy this code and/or
+  // change how it is built.
+# define PQCLEAN_PREVENT_BRANCH_HACK(b)  __asm__("" : "+r"(b) : /* no inputs */);
+#else
+# define PQCLEAN_PREVENT_BRANCH_HACK(b)
+#endif
+
 #endif // PQCLEAN_COMMON_COMPAT_H
