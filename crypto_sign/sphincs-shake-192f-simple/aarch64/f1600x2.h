815cbabfebb43df2e5186f2c0836cb7f54bd8c6c
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-02-17 14:20:26 +0800

    add namespacing to fips202x2

--- crypto_sign/sphincs-shake-192f-simple/aarch64/f1600x2.h
+++ crypto_sign/sphincs-shake-192f-simple/aarch64/f1600x2.h
@@ -1,8 +1,10 @@
 #ifndef SPX_F1600X2_H
 #define SPX_F1600X2_H
 
+#include "context.h"
+#include "params.h"
 #include <stdint.h>
-
+#define f1600_RC SPX_NAMESPACE(f1600_RC)
 extern uint64_t f1600_RC[24];
 extern void _f1600x2(uint64_t *a, uint64_t *rc);
 
