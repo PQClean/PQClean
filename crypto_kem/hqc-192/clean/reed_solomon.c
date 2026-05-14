b67545dad0cf27ccc057f94007952488242a9ccb
Chennamma <channuhotkar@gmail.com>
2026-05-14 13:02:31 +0000

    hqc: fix OOB read and ternary in compute_elp CT fix
    Extend syndromes array by 1 to allow safe CT read on final iteration.
    Replace ternary with pure bitwise mask to satisfy test_boolean policy.
    
    Signed-off-by: Chennamma <channuhotkar@gmail.com>

--- crypto_kem/hqc-192/clean/reed_solomon.c
+++ crypto_kem/hqc-192/clean/reed_solomon.c
@@ -134,7 +134,7 @@ static uint16_t compute_elp(uint16_t *sigma, const uint16_t *syndromes) {
         }
 
         deg_sigma_p ^= not_last & mask12 & (deg_sigma_copy ^ deg_sigma_p);
-        d = syndromes[mu + 1];
+        d ^= not_last & (syndromes[mu + 1] ^ d);
 
         for (i = 1; (i <= mu + 1) && (i <= PARAM_DELTA); ++i) {
             d ^= PQCLEAN_HQC192_CLEAN_gf_mul(sigma[i], syndromes[mu + 1 - i]);
@@ -304,7 +304,7 @@ static void correct_errors(uint8_t *cdw, const uint16_t *error_values) {
  * @param[in] cdw Array of size VEC_N1_SIZE_64 storing the received word
  */
 void PQCLEAN_HQC192_CLEAN_reed_solomon_decode(uint8_t *msg, uint8_t *cdw) {
-    uint16_t syndromes[2 * PARAM_DELTA] = {0};
+    uint16_t syndromes[2 * PARAM_DELTA + 1] = {0};
     uint16_t sigma[1 << PARAM_FFT] = {0};
     uint8_t error[1 << PARAM_M] = {0};
     uint16_t z[PARAM_N1] = {0};
