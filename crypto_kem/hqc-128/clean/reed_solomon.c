3e9c1411803465dff069c904373cc12ed13e1666
Chennamma <channuhotkar@gmail.com>
2026-05-14 11:51:16 +0000

    hqc: fix secret-dependent break in compute_elp
    Replace early break on final Berlekamp iteration with a constant-time
    not_last mask. Applies to HQC-128, HQC-192, HQC-256.
    
    Signed-off-by: Chennamma <channuhotkar@gmail.com>

--- crypto_kem/hqc-128/clean/reed_solomon.c
+++ crypto_kem/hqc-128/clean/reed_solomon.c
@@ -122,17 +122,15 @@ static uint16_t compute_elp(uint16_t *sigma, const uint16_t *syndromes) {
         mask12 = mask1 & mask2;
         deg_sigma ^= mask12 & (deg_X_sigma_p ^ deg_sigma);
 
-        if (mu == (2 * PARAM_DELTA - 1)) {
-            break;
-        }
+        uint16_t not_last = (uint16_t)(-((uint16_t)(mu - (2 * PARAM_DELTA - 1)) != 0));
 
-        pp ^= mask12 & (mu ^ pp);
-        d_p ^= mask12 & (d ^ d_p);
+        pp ^= not_last & mask12 & (mu ^ pp);
+        d_p ^= not_last & mask12 & (d ^ d_p);
         for (i = PARAM_DELTA; i; --i) {
-            X_sigma_p[i] = (mask12 & sigma_copy[i - 1]) ^ (~mask12 & X_sigma_p[i - 1]);
+            X_sigma_p[i] = (uint16_t)((not_last & ((mask12 & sigma_copy[i - 1]) ^ (~mask12 & X_sigma_p[i - 1]))) | (~not_last & X_sigma_p[i]));
         }
 
-        deg_sigma_p ^= mask12 & (deg_sigma_copy ^ deg_sigma_p);
+        deg_sigma_p ^= not_last & mask12 & (deg_sigma_copy ^ deg_sigma_p);
         d = syndromes[mu + 1];
 
         for (i = 1; (i <= mu + 1) && (i <= PARAM_DELTA); ++i) {
